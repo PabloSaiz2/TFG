8dc224a170b964710a1fa087a43fa74ebec1b2ac
pablo <pablocolme2@gmail.com>
2025-07-09 19:42:45 +0200

    Preparacion para Satie

---
+++ build/libompd/src/CMakeFiles/ompd.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pablo/Escritorio/TFG-repo/llvm-project/openmp/libompd/src/TargetValue.cpp" "libompd/src/CMakeFiles/ompd.dir/TargetValue.cpp.o" "gcc" "libompd/src/CMakeFiles/ompd.dir/TargetValue.cpp.o.d"
+  "/home/pablo/Escritorio/TFG-repo/llvm-project/openmp/libompd/src/omp-debug.cpp" "libompd/src/CMakeFiles/ompd.dir/omp-debug.cpp.o" "gcc" "libompd/src/CMakeFiles/ompd.dir/omp-debug.cpp.o.d"
+  "/home/pablo/Escritorio/TFG-repo/llvm-project/openmp/libompd/src/omp-icv.cpp" "libompd/src/CMakeFiles/ompd.dir/omp-icv.cpp.o" "gcc" "libompd/src/CMakeFiles/ompd.dir/omp-icv.cpp.o.d"
+  "/home/pablo/Escritorio/TFG-repo/llvm-project/openmp/libompd/src/omp-state.cpp" "libompd/src/CMakeFiles/ompd.dir/omp-state.cpp.o" "gcc" "libompd/src/CMakeFiles/ompd.dir/omp-state.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
